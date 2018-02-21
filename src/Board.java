641b0b0da067f5999ced194c33e7b63a2ef1b4e2
disow <disow@DESKTOP-GNC6CUT>
2018-02-21 14:48:51 -0500

    added board and location classes

---
+++ src/Board.java
@@ -0,0 +1,26 @@
+
+public class Board {
+
+	
+private int row;
+private int col;
+public Board() {
+	
+}
+
+public void createBoard() {
+	
+	Location[][] b = new Location[row][col];
+	for(int i=0;i<row;i++) {
+		for(int j=0;j<row;j++) {;
+			b[i][j] = new Location(null);
+		}
+	}
+}
+
+
+
+
+}
+	
+
---
+++ src/Location.java
@@ -0,0 +1,10 @@
+
+public class Location {
+
+private String name;
+public Location(String codename) {
+	name=codename;
+}
+
+
+}
