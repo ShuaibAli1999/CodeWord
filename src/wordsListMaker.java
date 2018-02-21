6d6d6f83dfbbbafdfb911fd87f1b560f958e0e40
disow <disow@DESKTOP-GNC6CUT>
2018-02-21 15:10:23 -0500

    added listmaker and listmakertest

---
+++ src/wordsListMaker.java
@@ -0,0 +1,39 @@
+
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+
+
+public class wordsListMaker {
+private String line;
+private ArrayList<String> words = new ArrayList<String>();
+
+public wordsListMaker() throws FileNotFoundException, IOException {
+
+
+	try (
+	    InputStream fis = new FileInputStream("Gamewords1.txt");
+	    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+	    BufferedReader br = new BufferedReader(isr);
+	) {
+	    while ((line = br.readLine()) != null) {
+	         words.add(line);
+	    }
+	}
+}
+
+public ArrayList<String> getList() {
+	
+	return words;
+}
+
+
+
+}
---
+++ src/wordsListMakerTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class wordsListMakerTest {
+
+
+@Test
+public void listCheckerTest() throws FileNotFoundException, IOException {
+	ArrayList<String> test = new ArrayList<String>();
+	wordsListMaker  l=  new wordsListMaker();
+	test = l.getList();
+	assertEquals("AFRICA",test.get(0));
+	assertEquals("AIR",test.get(2));
+	assertEquals("AMAZON",test.get(5));
+}
+
+}
