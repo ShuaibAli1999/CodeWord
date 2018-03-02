1d4ceeafdae4891b597557a2e28046edfec719a5
jenny <gcpierce@buffalo.edu>
2018-03-02 16:51:59 -0500

    added javadoc comments to PersonAssignments and CodenamesList classes

--- src/code/PersonAssignments.java
+++ src/code/PersonAssignments.java
@@ -3,51 +3,61 @@ package code;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * this class is used to create a shuffled arraylist of person assignments
+ * 
+ * @author jenny
+ */
+
 public class PersonAssignments {
 	
+	/**
+	 * an arraylist that will have 25 person assignments added to it and
+	 * shuffled to be returned by the method
+	 */
 	private ArrayList<String> retVal = new ArrayList<String>();
-	
+
+	/** 
+	 * this method adds 9 "red agents", 8 "blue agents", 7 "innocent bystanders"
+	 * and one assassin to an arraylist and then shuffles the entries into
+	 * different indexes for randomness
+	 * 
+	 * @return a shuffled arraylist of 25 person assignments
+	 */
 	public ArrayList<String> getList() {
 		
-		ArrayList<String> personList = new ArrayList<String>();
+		// adds 25 person assignments to an array list
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("red agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("blue agent");
+		retVal.add("innocent bystander");
+		retVal.add("innocent bystander");
+		retVal.add("innocent bystander");
+		retVal.add("innocent bystander");
+		retVal.add("innocent bystander");
+		retVal.add("innocent bystander");
+		retVal.add("innocent bystander");
+		retVal.add("assassin");
 		
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("red agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("blue agent");
-		personList.add("innocent bystander");
-		personList.add("innocent bystander");
-		personList.add("innocent bystander");
-		personList.add("innocent bystander");
-		personList.add("innocent bystander");
-		personList.add("innocent bystander");
-		personList.add("innocent bystander");
-		personList.add("assassin");
+		// shuffles elements within the arrayList to different indexes
+		Collections.shuffle(retVal);
 		
-		Collections.shuffle(personList);
-		
-		retVal = personList;
-		return retVal;
+		return retVal; // returns arraylist with all the persons shuffled
 		
 	}
-	
-//	public ArrayList<String> getList(){
-//		
-//		return retVal;
-//		
-//	}
 
 }
