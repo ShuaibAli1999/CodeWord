a1ba0a23ba5553c6f39ae87e62ae6f023c1d3abc
yingy <yingy@DESKTOP-VRKE50I>
2018-03-03 14:29:12 -0500

    add java docs to location and board class

--- src/code/Board.java
+++ src/code/Board.java
@@ -1,13 +1,22 @@
 package code;
+/**
+ * This is 5x5 grid space holder for location array.
+ */
 public class Board {
-
-	
+/**
+ * Set both the row and column to 5.
+ */
 private int row = 5;
 private int col = 5;
+
+/**
+ * This is a default constructor.
+ */
 public Board() {
 	
 }
 Location[][] b = new Location[row][col];
+
 public void createBoard() {
 	for(int i=0;i<row;i++) {
 		for(int j=0;j<row;j++) {;
@@ -15,11 +24,17 @@ public void createBoard() {
 		}
 	}
 }
-
+/**
+ * This is a getter method that return a location.
+ * @return a specific location from location array.
+ */
 public Location getLoc(int x, int y){
 	return b[x][y];
 }
-
+/**
+ * This is a getter method that return a location array.
+ * @return the location array
+ */
 public Location[][] getArray() {
 	return b;
 }
--- src/code/Location.java
+++ src/code/Location.java
@@ -1,12 +1,18 @@
 package code;
-
+/**
+ * This class create location instance that store the string codenames.
+ */
 public class Location {
-
 private String name;
+/**
+ * This is constructor assigning the instance variable name to the pass-in string.
+ */
 public Location(String codename) {
 	name=codename;
 }
-
+/**
+ * This is a getter method to get access of the string store in the Location instance variable.
+ */
 public String getName() {
 	return name;
 }
