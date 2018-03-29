85b7579ac5e35d7894ea9c73453c205c9b482802
jenny <gcpierce@buffalo.edu>
2018-03-29 14:08:00 -0400

    made a "file" menu with "new game" and "exit" drop down choices

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -8,6 +8,9 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
@@ -26,6 +29,17 @@ public class GUI implements Observer {
 		_windowHolder = driver;
 		_model = m;
 		JPanel _mainPanel = mp;
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenuItem menuItem;
+		menuBar = new JMenuBar();
+		mp.add(menuBar);
+		menu = new JMenu("File");
+		menuBar.add(menu);
+		menuItem = new JMenuItem("New Game");
+		menu.add(menuItem);
+		menuItem = new JMenuItem("Exit");
+		menu.add(menuItem);
 }
 
 	@Override
