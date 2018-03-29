b96cdb99bdac853c3174d7430517a1adcc6b307a
taojunzheng <taojunzheng@10.84.65.79>
2018-03-29 13:16:30 -0400

    Gui

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -1,5 +1,38 @@
 package Driver;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class Driver {
 
+import GUI.GUI;
+
+public class Driver implements Runnable {
+	
+	private JFrame _window;
+	private JPanel _mainPanel;
+	public Driver(Model m) {
+		_model = m;
+	}
+	
+	public static void main(String[] args) {
+		Model m = new Model("dictionaries/large.txt", "dictionaries/HW2.config" );
+		SwingUtilities.invokeLater(new Driver(m));
+	}
+
+	@Override
+	public void run() {
+		_window = new JFrame("TwistyWord");
+		_mainPanel = new JPanel();
+		_window.getContentPane().add(_mainPanel);
+
+		new GUI(_model, _mainPanel, this);
+		
+		_window.setVisible(true);
+		_window.pack();
+		_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
+	public void updateJFrame() {
+		_window.pack();
+		_window.repaint();
+	}
 }
